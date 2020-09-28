33d1b76d8f6d373bea8e3f162e2fedcd89a5f824
maulid <maulidabdullahi3@gmail.com>
2020-09-28 15:36:12 +0300

    Created Modules OneToOne Relationships

--- src/main/java/com/spring/Recipe/controllers/IndexController.java
+++ src/main/java/com/spring/Recipe/controllers/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-
+        System.out.println("Some message to say.....12345ssss");
         return "index";
     }
 
