1cf1b1ce97c44c280707f89e285071446ba8ef2b
maulid <maulidabdullahi3@gmail.com>
2020-09-28 17:15:17 +0300

    Database Initialization with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (description) VALUES ('Kenyan');
+INSERT INTO category (description) VALUES ('Ugandan');
+INSERT INTO category (description) VALUES ('Burundi');
+INSERT INTO category (description) VALUES ('Rwandan');
+INSERT INTO unit_of_measure (description) VALUES ('TeaSpoon');
+INSERT INTO unit_of_measure (description) VALUES ('TableSpoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Once');
